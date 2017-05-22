01076fd0ab27e5bd6d99a0880dc39527f9a6b264
usife <uchoullam@gmail.com>
2017-05-22 14:35:27 +0000

    lots

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
-    get "about", to: 'pages#about'
+    get "home", to: 'pages#home'
     get "about", to: 'pages#about'
     get "contact", to: 'pages#contact'
 end
